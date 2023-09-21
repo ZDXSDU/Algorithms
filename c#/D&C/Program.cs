cd4472b9bbd55923f47dada5afcbacc54bdba960
ZDXSDU <2398463125@qq.com>
2023-09-21 08:26:28 +0800

    利用递归 分而治之思想实现泛型集合SUM

---
+++ c#/D&C/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+int SumList(List<int> ints)
+{
+    if (ints.Count == 1)
+    {
+        return ints[0];
+    }
+    else
+    {
+        int temp = ints[0];
+        ints.RemoveAt(0);
+        return temp + SumList(ints);
+    }
+}
+
+int[] ints = { 1, 2147, 3, 654, 5, 6, 567, 8, 9, 120 };
+List<int> list = new List<int>(ints);
+Console.WriteLine(SumList(list));
+
+
+int sum = 0;
+for (int i = 0; i < ints.Length; i++)
+{
+    sum += ints[i];
+}
+Console.WriteLine(sum);
